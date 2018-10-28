050f3ba5f4dc67754d4bec1482dbb4813364f840
haya <haya@hayanomacbook-ea.local>
2018-10-28 20:29:49 +0900

    red background

---
+++ MyLibrary/MainTabBarController.swift
@@ -0,0 +1,17 @@
+//
+//  MainTabBarController.swift
+//  MyLibrary
+//
+//  Created by haya on 2018/10/28.
+//  Copyright © 2018年 haya. All rights reserved.
+//
+
+import UIKit
+
+class MainTabBarController: UITabBarController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .red
+    }
+}
